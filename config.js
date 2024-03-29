97714e3bc25b13252aec7db77b13bde1dfe9edd2
adcontreras <adcontrerasmm@gmail.com>
2024-03-28 21:29:22 -0600

    💄 User , model, controller & routes

--- config.js
+++ config.js
@@ -5,6 +5,13 @@ dotenv.config();
 
 const connect = () =>{
     mongoose.connect(process.env.DB_URI); // esta funcion ejecuta la conexción a mongo 
+    const connection = mongoose.connection;
+    connection.once('open', () => {  //callback --> es una función que se va a ejecutar después 
+        console.log('Conexión exitosa a la base de datos');
+    });
+    connection.once('error', () => {
+        console.error('Error al conectarse a la DB')
+    })
 }
 
 export {connect}
